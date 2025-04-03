25a7da92ff54737a01f103139b7ed7b2a25c8c97
ankitranjan1028 <aankitrn17@gmail.com>
2025-04-03 23:39:47 +0530

    logo change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,7 +35,7 @@ function NavBar() {
     >
       <Container>
         <Navbar.Brand href="/" className="d-flex">
-          <img src={logo} className="img-fluid logo" alt="brand" />
+          <img src={logo} className="img-fluid logo" style={{height:"80px", width:"100px"}} alt="brand" />
         </Navbar.Brand>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
